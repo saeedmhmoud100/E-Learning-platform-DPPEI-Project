f5153c1782a63061d9f8de2b407720c21988049f
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2024-08-31 22:47:39 +0300

    starting in the project and install some libraries

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
+import {Route, Routes} from "react-router-dom";
+import {Register} from "./Pages/Register";
+import {Login} from "./Pages/Login";
+import {Home} from "./Pages/Home";
+import {Navbar} from "./components/Navbar/Navbar";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
