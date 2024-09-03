6b82ca4472efe5f2a1b68b0bca97cad83a18b5ec
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2024-09-03 22:02:53 +0300

    create Courses Ordered page design

--- src/Pages/Admin/CoursesOrdered/index.js
+++ src/Pages/Admin/CoursesOrdered/index.js
@@ -1,7 +1,41 @@
 export const CoursesOrdered = () => {
     return (
-        <div>
+        <div className={'w-100 overflow-x-auto'}>
             <h4 >Courses Ordered:</h4>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Course Name</th>
+                        <th>Price</th>
+                        <th>Order Date</th>
+                        <th>Order Status</th>
+                        <th>User who ordered</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>React</td>
+                        <td>100</td>
+                        <td>2021-09-01</td>
+                        <td>Delivered</td>
+                        <td>John Doe</td>
+                    </tr>
+                    <tr>
+                        <td>Angular</td>
+                        <td>150</td>
+                        <td>2021-09-01</td>
+                        <td>Delivered</td>
+                        <td>John Doe</td>
+                    </tr>
+                    <tr>
+                        <td>Vue</td>
+                        <td>200</td>
+                        <td>2021-09-01</td>
+                        <td>Delivered</td>
+                        <td>John Doe</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
