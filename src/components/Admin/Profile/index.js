578f7fef4fa8c8e770771aeb78f456ba9d99e2c4
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2024-09-06 02:28:24 +0300

    create pie and polar components and add them in admin page and add some json files for data

--- src/components/Admin/Profile/index.js
+++ src/components/Admin/Profile/index.js
@@ -1,76 +1,54 @@
 import './style.css'
 import {ProfileCard} from "./ProfileCard";
-
+import {useEffect, useState} from "react";
+import {PieChart} from "../../Charts/PieChart";
+import {PolarChart} from "../../Charts/PolarChart";
 
 export const Profile = () => {
 
-    const data = [
-        {
-            title:'Courses Count',
-            data:[
-                {label:'Total', value:50},
-                {label:'Last Year', value:30},
-                {label:'Last Month', value:20}
-            ],
-            bgColor:'success',
-        },
-        {
-            title:'Orders Count',
-            data:[
-                {label:'Total', value:50},
-                {label:'Last Year', value:30},
-                {label:'Last Month', value:20}
-            ],
-            bgColor:'primary',
-        },
-        {
-            title:'Users Count',
-            data:[
-                {label:'Total', value:50},
-                {label:'Last Year', value:30},
-                {label:'Last Month', value:20}
-            ],
-            bgColor:'secondary',
-        },
-        {
-            title:'Instructors Count',
-            data:[
-                {label:'Total', value:50},
-                {label:'Last Year', value:30},
-                {label:'Last Month', value:20}
-            ],
-            bgColor:'info',
-        },
-        {
-            title:'Students Count',
-            data:[
-                {label:'Total', value:50},
-                {label:'Last Year', value:30},
-                {label:'Last Month', value:20}
-            ],
-            bgColor:'warning',
-        },
-        {
-            title:'Categories Count',
-            data:[
-                {label:'Total', value:50},
-                {label:'Last Year', value:30},
-                {label:'Last Month', value:20}
-            ],
-            bgColor:'danger',
-        },
-        ]
+    const [ProfileData, setProfileData] = useState([])
+    const [pieChartData, setPieChartData] = useState({})
+    const [polarChartData, setPolarChartData] = useState({})
+    useEffect(() => {
+        fetch('/data/admin/profile.json')
+            .then(response => response.json())
+            .then(data => {
+                setProfileData(data)
+            })
 
+        fetch('/data/admin/pieChartData.json')
+            .then(response => response.json())
+            .then(data => {
+                setPieChartData(data)
+            })
+
+        fetch('/data/admin/polarChartData.json')
+            .then(response => response.json())
+            .then(data => {
+                setPolarChartData(data)
+            })
+
+    },[])
 
     return (
         <div className='row g-3'>
 
-            {data.map((item, index) => (
+            {ProfileData?.map((item, index) => (
                 <div key={index} className="col-lg-6 col-12 text-white">
                     <ProfileCard data={item.data} title={item.title} backgroundColor={item.bgColor}/>
                 </div>
                 ))
             }
+
+            <h2>Analysis</h2>
+
+            <div className="col-6">
+                <PieChart data={pieChartData}/>
+            </div>
+            <div className="col-6">
+                <PolarChart data={polarChartData}/>
+            </div>
+
 
         </div>
     )
