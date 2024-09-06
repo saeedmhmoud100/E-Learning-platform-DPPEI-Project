36bdf8c9ebf18242e78102417baad0debd333bb6
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2024-09-06 03:44:21 +0300

    add barChart in published courses page in admin panel

--- src/components/Admin/PublishedCourses/index.js
+++ src/components/Admin/PublishedCourses/index.js
@@ -1,17 +1,31 @@
+import {BarChart} from "../../Charts/barChart";
+import {useEffect, useState} from "react";
+
 export const PublishedCourses = () => {
+    const [barChartData, setBarChartData] = useState({})
+
+    useEffect(() => {
+        fetch('/data/admin/barChartData.json')
+            .then(response => response.json())
+            .then(data => {
+                setBarChartData(data)
+            })
+    },[])
+
     return (
-        <div className='w-100 overflow-x-auto'>
-            <h4>Published Courses</h4>
-            <table className="table table-striped">
-                <thead>
+        <>
+            <div className='w-100 overflow-auto col-12' style={{maxHeight:"800px"}}>
+                <h4>Published Courses</h4>
+                <table className="table table-striped">
+                    <thead>
                     <tr>
                         <th scope='col'>#</th>
                         <th scope='col'>Course Name</th>
                         <th scope='col'>Published Date</th>
                         <th scope='col'>Published By</th>
                     </tr>
-                </thead>
-                <tbody>
+                    </thead>
+                    <tbody>
                     <tr>
                         <th scope='row'>1</th>
                         <td>React</td>
@@ -30,8 +44,16 @@ export const PublishedCourses = () => {
                         <td>2021-10-10</td>
                         <td>John Doe</td>
                     </tr>
-                </tbody>
-            </table>
-        </div>
+                    </tbody>
+                </table>
+            </div>
+
+
+            <div className="col-12">
+                <h2 className='mt-5'>Published Courses Analysis</h2>
+                    <BarChart data={barChartData}/>
+            </div>
+        </>
+
     )
 }
