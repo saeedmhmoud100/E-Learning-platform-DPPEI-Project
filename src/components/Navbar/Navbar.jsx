9b7bca87343f022d712eaab87afaffb87813a05b
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2024-09-07 15:15:26 +0300

    add authentication state, actions and user data in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,30 @@
 import {Link} from "react-router-dom";
 import './Navbar.css';
+import {useEffect, useState} from "react";
 
 export const Navbar = () => {
+
+
+
+    const [userData, setUserData] = useState({});
+
+    useEffect(() => {
+        fetch('/data/user/userData.json').then(response => response.json()).then(data => setUserData(data));
+    },[]);
+
+
+    function logout() {
+        setUserData({...userData, loggedIn: false, isAdmin: false})
+    }
+
+    function login() {
+        setUserData({...userData, loggedIn: true, isAdmin: false})
+    }
+
+    function changeAdmin() {
+        setUserData({...userData, loggedIn: true, isAdmin: !userData.isAdmin})
+    }
+
     
     return (
         <nav className="navbar navbar-expand-lg navbar-dark p-3 position-relative">
@@ -12,16 +35,23 @@ export const Navbar = () => {
                     <span className="navbar-toggler-icon"/>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
+
                     <ul className="navbar-nav">
                         <li className="nav-item">
                             <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                         </li>
-                        <li className="nav-item">
-                            <Link className="nav-link active" to="/courses">Courses</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link active" to="/admin">Admin</Link>
-                        </li>
+                        {
+                            userData.loggedIn ? (
+                                userData.isAdmin ?
+                                    <li className="nav-item">
+                                        <Link className="nav-link active" to="/admin">Admin</Link>
+                                    </li>
+                                    :
+                                    <li className="nav-item">
+                                        <Link className="nav-link active" to="/courses">Courses</Link>
+                                    </li>
+                            ):null
+                        }
 
                         <li className="nav-item dropdown">
                             <Link className="nav-link dropdown-toggle active" role="button" to="" id="navbarDropdown" data-bs-toggle="dropdown">
@@ -51,23 +81,37 @@ export const Navbar = () => {
                             <Link className="nav-link active" to="/courses"><i className="fa-solid fa-cart-shopping"></i></Link>
                         </li>
 
-                        <li className="nav-item dropdown me-3 ">
-                            <Link className="nav-link active dropdown-toggle" role="button" data-bs-toggle="dropdown" to="">
-                                <i className="fa-regular fa-bell"></i>
-                            </Link>
-                            <div className="dropdown-menu nav-dropdown-absolute">
-                                <Link className="dropdown-item" href="#">notification</Link>
-                            </div>
-                        </li>
+                        {
+                            userData.loggedIn ? <li className="nav-item dropdown me-3 ">
+                                <Link className="nav-link active dropdown-toggle" role="button" data-bs-toggle="dropdown" to="">
+                                    <i className="fa-regular fa-bell"></i>
+                                </Link>
+                                <div className="dropdown-menu nav-dropdown-absolute">
+                                    <Link className="dropdown-item" href="#">notification</Link>
+                                </div>
+                            </li> : null
+                        }
 
                         <li className="nav-item dropdown me-3   ">
                             <Link className="nav-link active dropdown-toggle"role="button" to='' data-bs-toggle="dropdown" aria-expanded="false">
                                 <i className="fa-regular fa-user"></i>
                             </Link>
                             <ul className="dropdown-menu nav-dropdown-absolute">
-                                <li><Link className="dropdown-item" to="#">Profile</Link></li>
-                                <li><Link className="dropdown-item" to="#">My Courses</Link></li>
-                                <li><Link className="dropdown-item" to="#">Purchase History</Link></li>
+                                {
+                                    userData.loggedIn ? (
+                                        <>
+                                            <li><Link className="dropdown-item" to="#">Profile</Link></li>
+                                            <li><Link className="dropdown-item" to="#">My Courses</Link></li>
+                                            <li><Link className="dropdown-item" to="#">Purchase History</Link></li>
+                                            <li onClick={changeAdmin}><Link className="dropdown-item" to="#">{userData.isAdmin ? "make user" : 'make admin'}</Link></li>
+                                            <li onClick={logout}><Link className="dropdown-item" to="#">logout</Link></li>
+                                        </> )
+                                        : (
+                                            <>
+                                                <li onClick={login}><Link className="dropdown-item" to="/login">Login</Link></li>
+                                                <li><Link className="dropdown-item" to="/register">Register</Link></li>
+                                            </>)
+                                }
                             </ul>
                         </li>
                     </ul>
