ef8800d5d72d98eaa37fa19ed8f41b1775a49b14
Dalia Adel <daliaadel262@gmail.com>
2024-09-14 11:50:41 +0300

    added courses route to app.jsx

--- src/App.js
+++ src/App.js
@@ -76,6 +76,7 @@ function App() {
             <Route path="/profile/courses" element={<UserCourses/>}/>
             <Route path="/profile/purchase-history" element={<PurchaseHistory/>}/>
             <Route path="/help-and-support" element={<HelpSupport/>}/>
+            <Route path='/courses' element={<Courses />}/>
             <Route path='/*' element={<NotFound/>}/>
         </Routes>
         <Footer />
