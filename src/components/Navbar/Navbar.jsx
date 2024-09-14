79eb1f343ac392362b112323c9078c050a6086c3
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2024-09-14 19:38:03 +0300

    redirect to courses page when search in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,16 @@
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import './Navbar.css';
 
 export const Navbar = ({logout, changeRule, userData}) => {
+    const navigate = useNavigate()
 
+
+
+
+    const handleSubmit = e =>{
+        e.preventDefault()
+        navigate('/courses')
+    }
 
     return (
         <nav className="navbar navbar-expand-lg navbar-dark p-3 position-relative">
@@ -59,7 +67,7 @@ export const Navbar = ({logout, changeRule, userData}) => {
 
                     <ul className="navbar-nav w-50">
                         <li className="nav-item ms-md-3 ms-auto w-100">
-                            <form className="form-inline my-2 my-lg-0">
+                            <form className="form-inline my-2 my-lg-0" onSubmit={handleSubmit}>
                                 <input className="form-control mr-sm-2 rounded-5" type="search" placeholder="Search"
                                        aria-label="Search"/>
                             </form>
