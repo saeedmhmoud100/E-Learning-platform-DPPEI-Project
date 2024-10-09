aa330ce1b80dbaaa74df98faa055ad3f972593a5
mariameid <mariameid210@gmail.com>
2024-10-09 03:59:06 +0200

    added dynamic data fetched from api

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -5,10 +5,5 @@ import searchReducer from './searchReducer'
 
 export default combineReducers ({
     allCourses:coursesReducer,
-<<<<<<< HEAD
-  
-=======
-    searchTerm: searchReducer,
->>>>>>> 3a3b559eaba47194b9e9155e303ca8d2311e57f6
 
 })
