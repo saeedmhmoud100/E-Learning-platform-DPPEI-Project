56bb2cc799b40862945fc5ecab1ed67d62072cf9
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2024-10-11 11:37:22 +0300

    remove JSON methods from token functions

--- src/hooks/myToken.js
+++ src/hooks/myToken.js
@@ -1,12 +1,12 @@
 import { useSelector } from "react-redux";
 
 export function setToken(token){
-    localStorage.setItem('token',JSON.stringify(token));
+    localStorage.setItem('token',token);
 }
 
 export function getToken(){
     const token = localStorage.getItem('token');
-    return JSON.parse(token);
+    return token;
 }
 
 export function removeToken(){
