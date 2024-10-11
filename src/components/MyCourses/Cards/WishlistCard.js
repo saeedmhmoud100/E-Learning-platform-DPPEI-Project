86f6ae57eba0f6dfa9f1e3cadb662d7968f9f8a8
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2024-10-11 18:06:27 +0300

    show user's wishlist with loading in my-courses page

--- src/components/MyCourses/Cards/WishlistCard.js
+++ src/components/MyCourses/Cards/WishlistCard.js
@@ -1,7 +1,7 @@
 import {useState} from "react";
 import {Link} from "react-router-dom";
 
-export const WishlistCard = () => {
+export const WishlistCard = ({course}) => {
     const [addedToWishlist, setAddToWishlist] = useState(false);
 
     function addToWishlist(){
@@ -14,22 +14,20 @@ export const WishlistCard = () => {
                 <div className="wishlist-icon" onClick={addToWishlist}>
                     <i className={`${addedToWishlist ? 'fa-regular' : 'fa-solid'} fa-heart`}></i>
                 </div>
-                <Link to={'/course-details'}>
+                <Link to={'/course-details/' + course.id}>
                 <img
-                    src="/images/coursePhoto.jpg"
+                    src={course.thumbnail_link}
                     className="card-img-top custom-card-img"
                     alt="React - The Complete Guide"
                 />
                 </Link>
                 <div className="card-body lh-sm p-2">
-                    <h6 className="card-title lh-sm custom-card-title">React - The Complete Guide 2024
-                        (incl. Next.js, Redux)</h6>
-                    <p className="card-text lh-sm custom-card-text">Academind by Maximilian
-                        Schwarzmüller</p>
-                    <p className="card-text lh-sm custom-card-text">4.6 (217,520)</p>
-                    <p className="card-text lh-sm custom-card-text">68.5 total hours • 697 lectures</p>
-                    <p className="text-muted lh-sm custom-card-text">£299.99 <span
-                        className="custom-price">£1,799.99</span></p>
+                    <h6 className="card-title lh-sm custom-card-title">{course.title}</h6>
+                    <p className="card-text lh-sm custom-card-text">{course.brief_description}</p>
+                    <p className="card-text lh-sm custom-card-text">{course.rating_avg || 0} ({course.rating_count})</p>
+                    <p className="card-text lh-sm custom-card-text">{course.total_duration} total hours • {course.lectures_count} lectures</p>
+                    <p className="text-muted lh-sm custom-card-text">£{course.price} <span
+                        className="custom-price">{ course.price_before_discount ?  "£" + course.price_before_discount : ""}</span></p>
                 </div>
             </div>
         </div>
