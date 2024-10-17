7afbca76c60ed9828acb47a9b848ef33e642397b
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2024-10-17 08:49:27 +0300

    remove redux-devtools-extension from the json file

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware } from 'redux'
 import {thunk} from 'redux-thunk';
 import rootReducer from './reducers/rootReducer'
-import { composeWithDevTools } from 'redux-devtools-extension'
 const initailState = {}
 
 const meddleware = [thunk]
